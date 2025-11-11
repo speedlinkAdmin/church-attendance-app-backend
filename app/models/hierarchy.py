bbba2fa273a32d3e76a28bc04349c381720e46ea
Edward Ndiyo <edward.ndiyo@speedlinkng.com>
2025-11-11 16:58:55 +0100

    fixing issues with the add groups

--- app/models/hierarchy.py
+++ app/models/hierarchy.py
@@ -1,6 +1,7 @@
 # app/models/hierarchy.py
 from app.extensions import db
 
+
 class State(db.Model):
     __tablename__ = 'states'
 
@@ -13,6 +14,14 @@ class State(db.Model):
 
     def __repr__(self):
         return f"<State {self.name}>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "code": self.code,
+            "leader": self.leader
+        }
 
 class Region(db.Model):
     __tablename__ = 'regions'
@@ -27,6 +36,15 @@ class Region(db.Model):
 
     def __repr__(self):
         return f"<Region {self.name}>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "code": self.code,
+            "leader": self.leader,
+            "state_id": self.state_id
+        }
 
 class District(db.Model):
     __tablename__ = 'districts'
@@ -43,6 +61,16 @@ class District(db.Model):
 
     def __repr__(self):
         return f"<District {self.name}>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "code": self.code,
+            "leader": self.leader,
+            "state_id": self.state_id,
+            "region_id": self.region_id
+        }
 
 class Group(db.Model):
     __tablename__ = 'groups'
@@ -61,6 +89,17 @@ class Group(db.Model):
     def __repr__(self):
         return f"<Group {self.name}>"
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "code": self.code,
+            "leader": self.leader,
+            "state_id": self.state_id,
+            "region_id": self.region_id,
+            "district_id": self.district_id
+        }
+
 class OldGroup(db.Model):
     __tablename__ = 'old_groups'
 
@@ -76,3 +115,92 @@ class OldGroup(db.Model):
 
     def __repr__(self):
         return f"<OldGroup {self.name}>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "code": self.code,
+            "leader": self.leader,
+            "state_id": self.state_id,
+            "region_id": self.region_id,
+            "district_id": self.district_id,
+            "group_id": self.group_id
+        }
+
+
+# class State(db.Model):
+#     __tablename__ = 'states'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     code = db.Column(db.String(20), unique=True, nullable=False)
+#     leader = db.Column(db.String(100), nullable=True)
+
+#     regions = db.relationship('Region', backref='state', lazy=True)
+
+#     def __repr__(self):
+#         return f"<State {self.name}>"
+
+# class Region(db.Model):
+#     __tablename__ = 'regions'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     code = db.Column(db.String(20), unique=True, nullable=False)
+#     leader = db.Column(db.String(100), nullable=True)
+
+#     state_id = db.Column(db.Integer, db.ForeignKey('states.id'), nullable=False)
+#     districts = db.relationship('District', backref='region', lazy=True)
+
+#     def __repr__(self):
+#         return f"<Region {self.name}>"
+
+# class District(db.Model):
+#     __tablename__ = 'districts'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     code = db.Column(db.String(20), unique=True, nullable=False)
+#     leader = db.Column(db.String(100), nullable=True)
+
+#     state_id = db.Column(db.Integer, db.ForeignKey('states.id'), nullable=False)
+#     region_id = db.Column(db.Integer, db.ForeignKey('regions.id'), nullable=False)
+
+#     groups = db.relationship('Group', backref='district', lazy=True)
+
+#     def __repr__(self):
+#         return f"<District {self.name}>"
+
+# class Group(db.Model):
+#     __tablename__ = 'groups'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     code = db.Column(db.String(20), unique=True, nullable=False)
+#     leader = db.Column(db.String(100), nullable=True)
+
+#     state_id = db.Column(db.Integer, db.ForeignKey('states.id'), nullable=False)
+#     region_id = db.Column(db.Integer, db.ForeignKey('regions.id'), nullable=False)
+#     district_id = db.Column(db.Integer, db.ForeignKey('districts.id'), nullable=False)
+
+#     old_groups = db.relationship('OldGroup', backref='group', lazy=True)
+
+#     def __repr__(self):
+#         return f"<Group {self.name}>"
+
+# class OldGroup(db.Model):
+#     __tablename__ = 'old_groups'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     code = db.Column(db.String(20), unique=True, nullable=False)
+#     leader = db.Column(db.String(100), nullable=True)
+
+#     state_id = db.Column(db.Integer, db.ForeignKey('states.id'), nullable=False)
+#     region_id = db.Column(db.Integer, db.ForeignKey('regions.id'), nullable=False)
+#     district_id = db.Column(db.Integer, db.ForeignKey('districts.id'), nullable=False)
+#     group_id = db.Column(db.Integer, db.ForeignKey('groups.id'), nullable=False)
+
+#     def __repr__(self):
+#         return f"<OldGroup {self.name}>"
